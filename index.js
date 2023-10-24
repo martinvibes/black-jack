d4b21bae102183ecf4123884ed6065286fd02509
martin machiebe <martinmachiebe21@gmail.com>
2023-10-23 14:43:51 -1100

    added some changes to the buttons

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function renderCard() {
   if (sum < 21) {
     message = "do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "wohoo you've got black jack !!!";  
+    message = "wohoo you've got black jack !!!"; 
     hasBlackJack = true;
   } else {
     message = "you're out of the game!";
@@ -34,8 +34,8 @@ function randomCard() {
   let random = Math.floor( Math.random() * 13 ) + 1
   if (random === 1) {
     return 11;
-  } else if (random === 10) {
-    return 11;
+  } else if (random > 10) {
+    return 10;
   } else {
     return random;
   }
@@ -43,8 +43,9 @@ function randomCard() {
 
 function newCard() {
   let card = randomCard();
-  sum += card
-  cards.push(card)
+  sum += card;
+  cards.push(card);
+  newCard();
 }
 
 START_GAME.addEventListener("click", () => {
@@ -53,14 +54,14 @@ START_GAME.addEventListener("click", () => {
   cards = [firstCard, secondCard];
   sum = firstCard + secondCard;
   isAlive = true;
-  renderCard()
+  renderCard();
 })
 
 NEW_CARD.addEventListener("click", () => {
   if (hasBlackJack === false && isAlive === true) {
-    newCard()
+    newCard();
   }
-  renderCard()
+  renderCard();
 })
 
 
